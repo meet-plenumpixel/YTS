92bbfe40e2e6dade817dce13dc3ec0362c3b985a
meet-github <meet.plenumpixel+github@gmail.com>
2022-09-19 22:49:58 +0530

    change database config to automate heroku-postgresql connection

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -4,10 +4,25 @@ Django settings for production environment
 # STDLIB LIBRARY
 import os
 
+# THIRDPARTY LIBRARY
+import dj_database_url
+import django_heroku
+
 # LOCALFOLDER LIBRARY
-from . import LOGGING
+from . import DATABASES, LOGGING, MIDDLEWARE, STATICFILES_STORAGE
 
 
+
+# add WhiteNoiseMiddleware
+MIDDLEWARE.insert(
+  MIDDLEWARE.index(
+    'django.middleware.security.SecurityMiddleware'
+  )+1,
+  'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # set logger for logging module
 LOGGING['loggers'].update({
@@ -22,5 +37,7 @@ LOGGING['loggers'].update({
     'propagate': True,
   },
 })
+
+DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
 
 __all__ = [i for i in dir() if i.isupper()]    # type: ignore
