0bf36c157229e9d7a71486df34854ed82637bee9
meet-github <meet.plenumpixel+github@gmail.com>
2022-09-20 13:33:23 +0530

    change import settings logic

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -2,18 +2,44 @@
 This module helps to manage environment settings.
 """
 
-# import only ENV variables (uppercase variable)
-from ._base import *  # isort: skip
+# STDLIB LIBRARY
+import logging
+
+# LOCALFOLDER LIBRARY
+from ._base import *
 
 
-# environment settings
-if DEBUG:  # type: ignore
-    try:
-        # LOCALFOLDER LIBRARY
-        from .local import *
-    # pylint: disable-next=:binary-op-exception
-    except ImportError or ModuleNotFoundError:
-        pass
-else:
-    # LOCALFOLDER LIBRARY
-    from .prod import *
+
+logger = logging.getLogger(__name__)
+
+
+try:
+  # LOCALFOLDER LIBRARY
+  from .prod import *
+  logger.info('production settings imported')
+
+except  ImportError or ModuleNotFoundError as e:
+  logger.error('production settings not available')
+
+
+try:
+  # LOCALFOLDER LIBRARY
+  from .local import *
+  logger.info('development settings imported')
+
+except ImportError or ModuleNotFoundError as e:
+  logger.warning('local settings not available')
+
+
+
+# # environment settings
+# if DEBUG:  # type: ignore
+#     try:
+#         # LOCALFOLDER LIBRARY
+#         from .local import *
+#     # pylint: disable-next=:binary-op-exception
+#     except ImportError or ModuleNotFoundError:
+#         pass
+# else:
+#     # LOCALFOLDER LIBRARY
+#     from .prod import *
