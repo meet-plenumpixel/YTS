e90149ac5b1e9e29918c1fb29441723d8fbea5fa
meet-github <meet.plenumpixel+github@gmail.com>
2022-09-30 23:44:03 +0530

    solve unable to get setting from database

--- multiapps/scraper/apps.py
+++ multiapps/scraper/apps.py
@@ -8,8 +8,11 @@ import os
 
 # DJANGO LIBRARY
 from django.apps import AppConfig
+from django.db.utils import OperationalError
 
 
+
+# from django.core.management import call_command
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +28,13 @@ class ScraperConfig(AppConfig):
     # FIRSTPARTY LIBRARY
     import scraper.signals
     from scraper.models import Setting
+    
+    try:
+      for key,value in Setting.objects.values_list("key", "value"):
+        os.environ[key] = value
+        logger.debug(f'SET {key}="{value}"')
+    except OperationalError as e:
+      logger.error('app level setting not imported from setting model')
+      # call_command('makemigrations')
+      # call_command('migrate')
 
-    for key,value in Setting.objects.values_list("key", "value"):
-      os.environ[key] = value
-      logger.debug(f'SET {key}="{value}"')
